ef44f3a1d4d9e5eeb6f11fa7876b647be7f1a56c
GreatOrator <github@dragontalk.net>
2014-07-29 14:41:05 -0700

    Day 2 update

--- src/main/java/com/greatorator/ddtc/DraconemDaemonion.java
+++ src/main/java/com/greatorator/ddtc/DraconemDaemonion.java
@@ -1,15 +1,17 @@
 package com.greatorator.ddtc;
 
-import com.greatorator.ddtc.configuration.ConfigurationHandler;
+import com.greatorator.ddtc.handler.ConfigurationHandler;
 import com.greatorator.ddtc.proxy.IProxy;
 import com.greatorator.ddtc.reference.Reference;
+import com.greatorator.ddtc.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class DraconemDaemonion
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -22,16 +24,20 @@ public class DraconemDaemonion
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        LogHelper.info("Pre Initialization Complete!");
     }
 
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialization Complete!");
     }
 
+    @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialization Complete!");
     }
 
 
