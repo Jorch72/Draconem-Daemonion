693097eafc0b4931decef4149bf323bc398c5095
GreatOrator <github@dragontalk.net>
2014-07-31 18:52:43 -0700

    Lookie lookie, who's got a cookie!

---
+++ src/main/java/com/greatorator/ddtc/creativetab/CreativeTabDDTC.java
@@ -0,0 +1,18 @@
+package com.greatorator.ddtc.creativetab;
+
+import com.greatorator.ddtc.init.ModItems;
+import com.greatorator.ddtc.reference.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class CreativeTabDDTC
+{
+    public static final CreativeTabs DDTC_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase())
+    {
+        @Override
+        public Item getTabIconItem()
+        {
+            return ModItems.dragonFlower;
+        }
+    };
+}
